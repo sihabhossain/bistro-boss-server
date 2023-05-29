6eb1785be79f4e336cf0fcc2a8226e0b62312cbe
Sihab Hossain <sihabhossain380@gmail.com>
2023-05-29 15:39:40 +0600

    done for now

--- index.js
+++ index.js
@@ -34,11 +34,39 @@ async function run() {
         const cartCollection = client.db('bistroDB').collection('carts');
 
         // users related apis
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
             const result = usersCollection.insertOne(user)
             res.send(result)
         })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+
+
+
 
         // menu related apis
         app.get('/menu', async (req, res) => {
